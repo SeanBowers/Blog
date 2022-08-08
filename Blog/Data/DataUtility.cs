93d1c858ca7a5630baca0345aaa28bfba4232b49
Sean Bowers <seanbowers14@gmail.com>
2022-08-08 17:09:09 -0500

    update

---
+++ Blog/Data/DataUtility.cs
@@ -0,0 +1,102 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+namespace Blog.Data
+{
+    public static class DataUtility
+    {
+        private const string _adminEmail = "seanbowers14@gmail.com";
+        private const string _modEmail = "theseanbowers@gmail.com";
+        private const string _adminRole = "Administrator";
+        private const string _modRole = "Moderator";
+
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            var databaseUri = new Uri(databaseUrl);
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+            return builder.ToString();
+        }
+        public static async Task SeedDataAsync(IServiceProvider svcProvider)
+        {
+            //Service: An instance of RoleManager.
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            //Migration: This is the programmatic equivalent to update-database.
+            await dbContextSvc.Database.MigrateAsync();
+
+            //Serivce: An instance of RoleManager.
+            var roleManagerSvc = svcProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            //Serivce: An instance of UserManager Service.
+            var userManagerSvc = svcProvider.GetRequiredService<UserManager<BlogUser>>();
+
+            //Seed Roles
+            await SeedRolesAsync(roleManagerSvc);
+            //Seed Users
+            await SeedUsersAsync(dbContextSvc, userManagerSvc);
+
+        }
+
+        private static async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            //If no _adminRole create one.
+            if (!await roleManager.RoleExistsAsync(_adminRole))
+            {
+                await roleManager.CreateAsync(new IdentityRole(_adminRole));
+            }
+            //If no _modeRole create one.
+            if (!await roleManager.RoleExistsAsync(_modRole))
+            {
+                await roleManager.CreateAsync(new IdentityRole(_modRole));
+            }
+        }
+        private static async Task SeedUsersAsync(ApplicationDbContext context, UserManager<BlogUser> userManager)
+        {
+            if (!context.Users.Any(u => u.Email == _adminEmail))
+            {
+                BlogUser adminUser = new()
+                {
+                    Email = _adminEmail,
+                    UserName = _adminEmail,
+                    FirstName = "Sean",
+                    LastName = "Bowers",
+                    PhoneNumber = "7574038968",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(adminUser, "1qaz2wsx#EDC$RFV");
+                await userManager.AddToRoleAsync(adminUser, _adminRole);
+            }
+            if (!context.Users.Any(u => u.Email == _modEmail))
+            {
+                BlogUser modUser = new()
+                {
+                    Email = _modEmail,
+                    UserName = _modEmail,
+                    FirstName = "Blog",
+                    LastName = "Moderator",
+                    PhoneNumber = "7574038968",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(modUser, "1qaz2wsx#EDC$RFV");
+                await userManager.AddToRoleAsync(modUser, _modRole);
+            }
+        }
+    }
+}
--- Blog/Data/Migrations/20220808204152_001-Initial.Designer.cs
+++ Blog/Data/Migrations/20220808204152_001-Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Blog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Blog.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220808204152_001-Initial")]
+    partial class _001Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
