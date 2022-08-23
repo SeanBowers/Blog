17468ce9d143f96629667428784e4d2290665fcc
Sean Bowers <seanbowers14@gmail.com>
2022-08-23 16:10:39 -0500

    updates

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -4,13 +4,10 @@ using Blog.Services;
 using Blog.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-});
 
 // Add services to the container.
 //Define a db connection - DefaultConnection defined in app secrets. Pulled from DataUtility helper.
@@ -31,15 +28,54 @@ builder.Services.AddIdentity<BlogUser, IdentityRole>(options => options.SignIn.R
     .AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+//Services
 builder.Services.AddScoped<IImageService, ImageService>();
 builder.Services.AddScoped<IBlogService, BlogService>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
+// Google Auth
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+});
+
 builder.Services.AddMvc();
 
+// Swagger interface for API access
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Blog API",
+        Version = "v1",
+        Description = "Serving up Blog data using .Net 6",
+        Contact = new OpenApiContact
+        {
+            Name = "S.Bowers",
+            Email = "seanbowers14@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/sean-bowers/")
+        }
+    });
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddCors(obj =>
+{
+    obj.AddPolicy("DefaultPolicy",
+        builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
 var app = builder.Build();
+
+app.UseCors("DefaultPolicy");
 
 var scope = app.Services.CreateScope();
 await DataUtility.SeedDataAsync(scope.ServiceProvider);
@@ -56,6 +92,15 @@ else
     app.UseHsts();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PublicAPI v1");
+    c.InjectStylesheet("/css/swagger.css");
+    c.InjectJavascript("/js/swagger.js");
+    c.DocumentTitle = "Blog Public API";
+});
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -67,7 +112,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "Custom",
     pattern: "Content/{slug}",
-    defaults: new { controller = "BlogPosts", action = "Details"}
+    defaults: new { controller = "BlogPosts", action = "Details" }
     );
 
 app.MapControllerRoute(
