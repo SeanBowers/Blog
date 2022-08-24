a0b1862b29bce2d2fe4c0684d4187d530873e853
Sean Bowers <seanbowers14@gmail.com>
2022-08-24 13:08:08 -0500

    update

--- Blog/Data/DataUtility.cs
+++ Blog/Data/DataUtility.cs
@@ -38,7 +38,7 @@ namespace Blog.Data
                 Username = userInfo[0],
                 Password = userInfo[1],
                 Database = databaseUri.LocalPath.TrimStart('/'),
-                SslMode = SslMode.Prefer,
+                SslMode = SslMode.Require,
                 TrustServerCertificate = true
             };
             return builder.ToString();
